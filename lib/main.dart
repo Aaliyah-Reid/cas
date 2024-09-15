844926f4f004a2f4cfd1d513ac50ae5e175bfb58
RachWill314 <rachelle.williams314@gmail.com>
2024-09-15 00:05:22 -0500

    Cultural ToolKit Page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:cas/onboarding.dart';
+import 'package:cas/cultural.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const OnboardingPage()
+        home: CulturalToolkitPage()
     );
   }
 }
